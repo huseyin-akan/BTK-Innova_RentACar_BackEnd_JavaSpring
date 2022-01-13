d242beb165fd7677ad111a0c465e439e00fa0f59
husokanus <husokanus@gmail.com>
2022-01-14 02:00:03 +0300

    13.01.2022

--- app/src/main/java/com/btkAkademi/rentACar/dataAccess/abstracts/CarDao.java
+++ app/src/main/java/com/btkAkademi/rentACar/dataAccess/abstracts/CarDao.java
@@ -2,7 +2,6 @@ package com.btkAkademi.rentACar.dataAccess.abstracts;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
